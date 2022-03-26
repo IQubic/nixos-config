488a45731a8a55ab7beb9b33af835fcf71c524f4
iqubic <avi.caspe@gmail.com>
2022-03-26 14:20:24 -0700

    Add additional XMonad related files

---
+++ hm/xmonad.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+let
+  xmonadConfig = pkgs.haskellPackages.callCabal2nix "xmonad-config" ./xmonad-config {};
+in 
+{
+  home.file.".xmonad/xmonad-x86_64-linux".source = "${xmonadConfig}/bin/xmonad-config";
+}

d466b8eaf518c456308a787cf13579fef1351aef
iqubic <avi.caspe@gmail.com>
2022-03-26 23:13:08 -0700

    cleaning up comments

--- hm/xmonad-config/xmonad.nix
+++ hm/xmonad-config/xmonad.nix
@@ -3,6 +3,6 @@ let
   xmonadConfig = pkgs.haskellPackages.callCabal2nix "xmonad-config" ./. {};
 in 
 {
-  # Allow XMonad to my specific config
+  # Allow XMonad to my specific config when hot-reloading
   home.file."~/.cache/xmonad/xmonad-${pkgs.stdenv.system}".source = "${xmonadConfig}/bin/xmonad-config";
 }

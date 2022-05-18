9f6fd5f334bd17c38b27ef73dc9a85ead9bfdaa5
iqubic <avi.caspe@gmail.com>
2022-05-17 21:32:41 -0700

    More Doom-Emacs setup

--- hm/emacs/emacs.nix
+++ hm/emacs/emacs.nix
@@ -1,18 +1,17 @@
 # Doom Emacs via:
 # https://github.com/haedosa/nix-doom-emacs
-
 { config, pkgs, ... }:
 
-imports = [ nix-doom-emacs.hmModule ];
+{
+  # Enable Doom-Emacs
+  programs.doom-emacs = {
+    enable = true;
+    doomPrivateDir = ./doom.d;
+  };
 
-# Enable Doom-Emacs
-programs.doom-emacs = {
-  enable = true;
-  doomPrivateDir = ./doom.d;
-};
-
-# Emacs Daemon
-services.emacs = {
-  enable = true;
-  package = programs.emacs.package;
+  # Emacs Daemon
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs;
+  };
 }

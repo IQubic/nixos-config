e1856643da8f07fa3074ed93b13a729fe0bdd017
iqubic <avi.caspe@gmail.com>
2022-07-14 20:33:58 -0700

    update flake inputs

--- hm/picom.nix
+++ hm/picom.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 {
   services.picom = {
-    enable = true;
-
+    enable           = true;
     shadow           = false;
-    blur             = false;
-    activeOpacity    = "1.0";
-    inactiveOpacity  = "1.0";
-    menuOpacity      = "1.0";
+    activeOpacity    = 1.0;
+    inactiveOpacity  = 1.0;
+    menuOpacity      = 1.0;
   };
 }

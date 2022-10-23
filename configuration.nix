ff95c16a286158ba9178676664b215693382d074
iqubic <avi.caspe@gmail.com>
2022-10-22 20:17:13 -0700

    fix compilation errors

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 
 {
   # Use latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;

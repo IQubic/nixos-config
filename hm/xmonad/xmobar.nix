760534bed444489bfe6e54028fa05f0873f4b0a2
Sophia Caspe <avi.caspe@gmail.com>
2025-09-18 19:13:41 -0700

    Attempt to add battery settings back in

--- hm/xmonad/xmobar.nix
+++ hm/xmonad/xmobar.nix
@@ -3,8 +3,8 @@
   programs.xmobar.enable = true;
 
   home.file.".config/xmobar/xmobarrc_main".text = ''
-    Config { font = "Hack Mono 12"
-           , additionalFonts = ["Symbols Nerd Font Mono 12"]
+    Config { font = "Hack Mono 14"
+           , additionalFonts = ["Symbols Nerd Font Mono 16"]
            , borderColor = "black"
            , border = FullB
            , bgColor = "#1e1e2e"
@@ -20,10 +20,24 @@
            , textOutputFormat = Ansi
            , commands = [ Run Date "<fc=#cba6f7>%a %b %e - %H:%M:%S</fc>" "date" 10
                         , Run XPropertyLog "_XMONAD_LOG_1"
+                        , Run BatteryP ["BAT0"]
+                                       ["-t", "<acstatus>"
+                                       , "-S", "Off", "-d", "0", "-m", "3"
+                                       , "-L", "10", "-H", "90", "-p", "3"
+                                       , "-W", "0"
+                                       , "-f", "󰂎󰁺󰁻󰁼󰁽󰁾󰁿󰂀󰂁󰂂󰁹"
+                                       , "--"
+                                       , "-P"
+                                       , "-i", "<fn=1>\xf1e6</fn>"
+                                       , "-O", "<fn=1><leftbar>  \xf1e6</fn> <timeleft>"
+                                       , "-o", "<fn=1><leftbar></fn> <timeleft>"
+                                       , "-l", "#f38ba8" "-h" "#a6e3a1"
+                                       , "-H", "10", "-L", "7"
+                                       ] 50
                         ]
            , sepChar = "%"
            , alignSep = "}{"
-           , template = "<fn=1>\uf313</fn> %_XMONAD_LOG_1%}%date%{%battery% <fn=1>\uf35e</fn>"
+           , template = "<fn=1></fn> %_XMONAD_LOG_1%}%date%{%battery% <fn=1></fn>"
            }
   '';
 }

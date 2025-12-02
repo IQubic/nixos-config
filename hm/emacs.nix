542455d231e39470614ff84c87138a25e5b366a7
Sophia Caspe <avi.caspe@gmail.com>
2025-12-01 20:15:30 -0800

    emacs: add tree-sitter support

--- hm/emacs.nix
+++ hm/emacs.nix
@@ -4,9 +4,18 @@
   # Enable Emacs and Service
   programs.emacs = {
     enable = true;
+    extraPackages =
+    epkgs: with epkgs; ([
+      tree-sitter-langs
+      (treesit-grammars.with-grammars (p: [
+        p.tree-sitter-haskell
+        p.tree-sitter-nix
+      ]))
+    ]);
   };
   services.emacs.enable = true;
 
+  # Add Doom executable to the path
   home.sessionPath = [ "$HOME/.emacs.d/bin" ];
 
   # Packages required for Doom Emacs
